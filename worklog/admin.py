af3dd2b92631eac1699e8f38f02745172bd50ece
Marcin Nowak <marcin.j.nowak@gmail.com>
2019-06-02 20:38:53 +0200

    filter worklog by invoices

--- worklog/admin.py
+++ worklog/admin.py
@@ -5,10 +5,32 @@ import itertools
 from django.contrib import admin
 from django.db import transaction
 from django.shortcuts import redirect
+from django.utils.translation import ugettext_lazy as _
 
 from stock.models import Tax, Unit
 from invoices.admin import LineForm
 from .models import Entry
+
+
+class BooleanRelatedFieldFilter(admin.BooleanFieldListFilter):
+    def queryset(self, request, queryset):
+        if self.lookup_val == '1':
+            return queryset.filter(**{'%s__isnull' % self.field_path: False})
+        elif self.lookup_val == '0':
+            return queryset.filter(**{'%s__isnull' % self.field_path: True})
+        return queryset
+
+    def choices(self, changelist):
+        for lookup, title in (
+                (None, _('All')),
+                ('1', _('Wystawione')),
+                ('0', _('Nie wystawione'))):
+            yield {
+                'selected': self.lookup_val == lookup and not self.lookup_val2,
+                'query_string': changelist.get_query_string({
+                    self.lookup_kwarg: lookup}, [self.lookup_kwarg2]),
+                'display': title,
+            }
 
 
 @admin.register(Entry)
@@ -17,7 +39,11 @@ class EntryAdmin(admin.ModelAdmin):
             'title', 'customer', 'ticket', 'associate_role',
             'price', 'cost', 'profit',
             'duration', 'work_finished_at', 'invoice', 'invoiced', 'paid')
-    list_filter = ('customer', 'associate_role')
+    list_filter = (
+            ('invoice', BooleanRelatedFieldFilter),
+            ('customer', admin.RelatedOnlyFieldListFilter),
+            ('associate_role', admin.RelatedOnlyFieldListFilter),
+            )
     actions = ['make_invoices']
 
     def invoiced(self, obj):
